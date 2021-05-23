a1c121cd03251e212f884fc4f06fc6dd3d02659c
v.grigoryev <v.grigoryev@fun-box.ru>
2021-05-23 14:12:37 +0300

    Добавляет базовую реализацию convert.

--- lib/currency_converter.ex
+++ lib/currency_converter.ex
@@ -1,18 +1,12 @@
 defmodule CurrencyConverter do
-  @moduledoc """
-  Documentation for `CurrencyConverter`.
-  """
+  alias CurrencyConverter.CBRClient
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CurrencyConverter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec convert(float(), String.t(), Date.t()) :: {:ok, float} | {:error, atom()}
+  def convert(amount, currency_iso, date) do
+    with {:ok, rates} <- CBRClient.request_cbr(date),
+         {:ok, rate} <- Map.fetch(rates, currency_iso)
+    do
+      {:ok, amount * rate}
+    end
   end
 end
